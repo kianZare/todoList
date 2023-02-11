59bfb332bf7080e2e29f36bb95d8d53b7d74ceaf
Kian Zareinajad <kzareinajad@gmail.com>
2023-02-11 02:14:11 +0000

    local storege

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const filterOption = document.querySelector(".filter-todos");
 //  Event Listeners :
 todoBtn.addEventListener("click", addTodo);
 todoList.addEventListener('click', checkRemove);
-filterOption.addEventListener("click", filterTodos)
+filterOption.addEventListener("click", filterTodos);
+document.addEventListener('DOMContentLoaded', getLocalTodos);
 
 
 // Functions :
@@ -25,19 +26,21 @@ function addTodo (e) {
     todoDiv.innerHTML = newTodo;
     // append to todolist:
     todoList.appendChild(todoDiv);
+    saveLocalTodos(todoInput.value);
     todoInput.value = "";
 };
 
 function checkRemove (e) {
     const classList = [...e.target.classList];
     const item = e.target;
-    console.log(item.parentElement.parentElement)
+    // console.log(item.parentElement.parentElement)
     
     if(classList[1] === "fa-check-square") {
         const todo = item.parentElement.parentElement;
         todo.classList.toggle("completed")
     } else if (classList [1] === "fa-trash-alt"){
         const todo = item.parentElement.parentElement;
+        removeLocalTodos(todo);
         todo.remove();
     }
 };
@@ -71,3 +74,44 @@ function filterTodos (e) {
 };
 
 
+// local storege
+
+function saveLocalTodos (todo) {
+
+    let savedTodos = localStorage.getItem("todos")
+        ? JSON.parse(localStorage.getItem("todos"))
+        : [];
+    savedTodos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(savedTodos));
+}
+
+
+function getLocalTodos () {
+
+    let savedTodos = localStorage.getItem("todos")
+        ? JSON.parse(localStorage.getItem("todos"))
+        : [];
+    savedTodos.forEach((todo) => {
+        const todoDiv = document.createElement('div');
+        todoDiv.classList.add('todo');
+        const newTodo = `
+        <li>${todo}</li>
+        <span><i class="far fa-check-square"></i></span>
+        <span><i class="far fa-trash-alt"></i></span>`;
+        todoDiv.innerHTML = newTodo;
+        // append to todoList
+        todoList.appendChild(todoDiv);
+    
+    })
+}
+
+function removeLocalTodos(todo) {
+    console.log(todo.children[0].inerText);
+
+    let savedTodos = localStorage.getItem("todos")
+        ? JSON.parse(localStorage.getItem("todos"))
+        : [];
+    const filteredTodos = savedTodos.filter(
+        (t) => t !== todo.children[0].innerHTML);
+    localStorage.setItem("todos", JSON.stringify(filteredTodos));
+}
